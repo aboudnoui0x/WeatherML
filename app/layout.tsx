d386e2fe9acd0b6815eed0cb7b6ded392312b6a2
AboudNoui <abdallah_noui@univ-biskra.dz>
2025-10-24 14:31:20 +0100

    none

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'WeatherML',
+  description: 'Created by by Team AbdAllah Noui, Megueddem Yacine, and KHADRAOUI AYA ',
 }
 
 export default function RootLayout({
